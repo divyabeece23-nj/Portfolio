7aec3c2f3af5fffc3a3b18732e030a6cf331f39f
Divya Natarajan <divyabeece23@gmail.com>
2026-03-18 21:27:41 -0400

    added links for the project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import './styles/main.scss'
-// import Cursor   from './components/Cursor/Cursor'
-import Navbar   from './components/Navbar/Navbar'
-import Hero     from './components/Hero/Hero'
-import About    from './components/About/About'
-import Skills   from './components/Skills/Skills'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import About from './components/About/About'
+import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
-import Contact  from './components/Contact/Contact'
-import Footer   from './components/Footer/Footer'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 export default function App() {
   return (
     <>
-      {/* <Cursor /> */}
       <Navbar />
       <main>
         <Hero />
